10ff2119603ac2249e4e319d7be4af4259e00db2
Ashish Bhosle <asbhosle2003@gmail.com>
2024-08-02 00:27:46 +0530

    Navbar created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import CodeEditor from "@/components/editor";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div className="flex w-screen overflow-hidden overscroll-none flex-col h-screen bg-background">
-      <div className="h-12 flex">Navbar</div>
+      <div className="h-12 flex"><Navbar /></div>
       <div className="w-screen flex grow">
         <CodeEditor />
 
 
       </div>
     </div>
-  );
-}
+    );
+  }
---
+++ components/navbar/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Pencil } from "lucide-react";
+import Logo from "@/assets/logo.svg";
+import Image from "next/image";
+
+import Link from "next/link";
+
+
+const Navbar = () => {
+  return (
+    <div className="h-14 px-2 w-full border-b border-border flex items-center justify-between">
+    <div className="flex items-center space-x-4">
+      <Link
+        href={"/"}
+        className="ring-offset-2 ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none rounded-sm"
+      >
+        <Image src={Logo} alt="Logo" width={36} height={36} />
+      </Link>
+      <div className="text-sm font-medium flex items-center">Virtualbox</div>
+    </div>
+    <div className="flex items-center space-x-4">
+      
+    </div>
+  </div>
+  );
+};
+
+export default Navbar;
